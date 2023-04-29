2cc442c088390aaa9c8f21bf3d55b168f55e072e
むずでょ <muzudho1@gmail.com>
2023-04-30 08:52:38 +0900

    Update pipeline.bat

--- pipeline.bat
+++ pipeline.bat
@@ -1,5 +1,8 @@
+REM Reference / Make command for PyTorch install: [Get Started](https://pytorch.org/get-started/locally/)
+REM             Example: `conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia`
+REM Reference / FOR statement: [MS-DOS .bat control flow for loop](https://www.ipentec.com/document/ms-dos-bat-control-flow-for-loop)
 FOR /L %%v IN (1,1,100) DO (
-    python selfplay_main.py --save-dir archive --model model/rl-model.bin --use-gpu true
-    python get_final_status.py
-    python train.py --rl true --kifu-dir archive
+    python ./selfplay_main.py --save-dir archive --model model/rl-model.bin --use-gpu true
+    python ./get_final_status.py
+    python ./train.py --rl true --kifu-dir archive
 )
